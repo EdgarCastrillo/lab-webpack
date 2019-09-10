bd1d8c7c4e154863e12d1da419343705e73bc8c6
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-10 11:33:45 +0200

    npm install webpack babel scss

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '../css/style.scss'
+
+const cart = ['Product1', 'Product2', 'Product3','Producto4']
+
+cart.push('iPhone','iPad')
+
+console.log(cart)
+
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'},
+        ]
+      }
+    ]
+  }
+};
